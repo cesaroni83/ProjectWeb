aa5c14e4d3a1510669d02419f72ad4a723c77b09
cesar <cesar@ADMINISTRATOR>
2025-10-23 11:42:09 +0200

    ultimo

--- ProjectWeb/ProjectWeb.API/Controllers/AccountsController.cs
+++ ProjectWeb/ProjectWeb.API/Controllers/AccountsController.cs
@@ -155,42 +155,7 @@ namespace ProjectWeb.API.Controllers
             return Ok(await _userHelper.GetUserAsync(User.Identity!.Name!));
         }
 
-        //[HttpGet("all")]
-        //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        //public async Task<ActionResult> GetAll([FromQuery] PaginationDTO pagination)
-        //{
-        //    var queryable = _context.Users
-        //        .Include(u => u.City)
-        //        .AsQueryable();
-
-        //    if (!string.IsNullOrWhiteSpace(pagination.Filter))
-        //    {
-        //        queryable = queryable.Where(x => x.FirstName.ToLower().Contains(pagination.Filter.ToLower()) ||
-        //                                         x.LastName.ToLower().Contains(pagination.Filter.ToLower()));
-        //    }
-
-        //    return Ok(await queryable
-        //        .OrderBy(x => x.FirstName)
-        //        .ThenBy(x => x.LastName)
-        //        .Paginate(pagination)
-        //        .ToListAsync());
-        //}
-
-        //[HttpGet("totalPages")]
-        //public async Task<ActionResult> GetPages([FromQuery] PaginationDTO pagination)
-        //{
-        //    var queryable = _context.Users.AsQueryable();
-
-        //    if (!string.IsNullOrWhiteSpace(pagination.Filter))
-        //    {
-        //        queryable = queryable.Where(x => x.FirstName.ToLower().Contains(pagination.Filter.ToLower()) ||
-        //                                         x.LastName.ToLower().Contains(pagination.Filter.ToLower()));
-        //    }
-
-        //    double count = await queryable.CountAsync();
-        //    double totalPages = Math.Ceiling(count / pagination.RecordsNumber);
-        //    return Ok(totalPages);
-        //}
+       
         [AllowAnonymous]
         [HttpPost("CreateUser")]
         public async Task<ActionResult> CreateUser([FromBody] UserDTO model)
